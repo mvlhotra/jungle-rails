f9a26aa1bbb56bb3fb9eb36aff5bcda9588cd00c
Nik Malhotra <nmal@me.com>
2019-03-21 16:40:23 +0000

    Add session handling * signed up users can log in and out

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,9 @@ Rails.application.routes.draw do
 
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
 
 
   resources :orders, only: [:create, :show]
