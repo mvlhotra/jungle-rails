d0c705a8843c9081957aef3ead6fb0f55b0770e0
Nik Malhotra <nmal@me.com>
2019-03-26 04:21:33 +0000

    completed product model spec

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,23 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  category = Category.find_by(name: 'Electronics')
-  subject {described_class.new(name: "Airpods", price: 99.99, quantity: 99, category: category)}
+  test_category = Category.new(name: 'Drinks')
+  subject {
+    test_category.products.new(name: 'Coke Zero', price: 1.79, quantity: 1)
+  }
   describe 'Validations' do
-    # it "is valid with valid attributes" do
-    #   expect(subject).to be_valid
-    # end
+    it "is valid with valid attributes" do
+      expect(subject).to be_valid
+    end
     it "is not valid without a name" do
-      subject.name = nil
-      expect(subject).to_not be_valid
+      test_product = test_category.products.new(price: 2.99, quantity: 666)
+      test_product.save
+      expect(test_product.errors.full_messages.include? "Name can't be blank")
     end
     it "is not valid without a price" do
-      subject.price = nil
-      expect(subject).to_not be_valid
+      test_product = test_category.products.new(name: 'Surge', quantity: 3)
+      test_product.save
+      expect(test_product.errors.full_messages.include? "Price can't be blank")
     end
     it "is not valid without a quantity" do
-      subject.quantity = nil
-      expect(subject).to_not be_valid
+      test_product = test_category.products.new(name: 'OG Fanta', price: 2.99)
+      test_product.save
+      expect(test_product.errors.full_messages.include? "Quantity can't be blank")
     end
     it "is not valid without a category" do
       subject.category = nil
